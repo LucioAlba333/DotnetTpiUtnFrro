32411c5998fcb57fa9e79e630acfc8be767d771d
lucio <lucioalba02@gmail.com>
2025-08-02 16:23:51 -0300

    crud Especialidad

---
+++ Academia/Academia.Data/EspecialidadData.cs
@@ -0,0 +1,60 @@
+﻿using Academia.Models;
+
+namespace Academia.Data;
+
+public class EspecialidadData
+{
+	public static readonly List<Especialidad> especialidades = [];
+
+	public static void New(Especialidad especialidad)
+	{
+		especialidades.Add(especialidad);
+	}
+
+	public static bool Delete(int id)
+	{
+		Especialidad? especialidad =
+			especialidades.Find(x => x.Id == id);
+		if (especialidad != null)
+		{
+			especialidades.Remove(especialidad);
+			return true;
+		}
+		else
+			return false;
+	}
+	public static Especialidad? Get(int id)
+	{
+		Especialidad? e = especialidades.Find(x => x.Id == id);
+
+		if (e != null)
+		{
+			return new Especialidad(e);
+		}
+		else
+			return null;
+	}
+	public static IEnumerable<Especialidad> GetAll()
+	{
+		return especialidades.Select(e => new Especialidad(e)).ToList();
+	}
+	public static bool Update(Especialidad especialidad)
+	{
+		Especialidad? e =
+			especialidades.Find(x => x.Id == especialidad.Id);
+		if (e != null)
+		{
+			e.State = especialidad.State;
+			e.Descripcion = especialidad.Descripcion;
+			return true;
+		}
+		else
+			return false;
+
+	}
+
+
+
+
+
+}
---
+++ Academia/Academia.Models/BusinessEntity.cs
@@ -0,0 +1,20 @@
+﻿namespace Academia.Models;
+
+public abstract class BusinessEntity
+{
+	public BusinessEntity(int id)
+	{
+		if (id < 0)
+			throw new ArgumentException("El id debe ser mayor a 0.", nameof(id));
+		Id = id;
+		State = States.Activo;
+	}
+	public BusinessEntity(BusinessEntity entity)
+	{
+		this.Id = entity.Id;
+		this.State = entity.State;
+	}
+
+	public int Id { get; set; }
+	public States State { get; set; }
+}
