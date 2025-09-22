0cca6cdbbad513be0070333e778420d24c1db373
lucio <lucioalba02@gmail.com>
2025-09-21 21:24:07 -0300

    agregado usuario

---
+++ Academia/Academia.Dtos/PersonaDto.cs
@@ -0,0 +1,15 @@
+namespace Academia.Dtos;
+
+public class PersonaDto
+{
+    public int Id { get; set; }
+    public string Nombre { get; set; } = string.Empty; 
+    public string Apellido { get; set; } = string.Empty;
+    public string Direccion { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public int Legajo { get; set; }
+    public DateTime FechaNacimiento { get; set; }
+    public int IdPlan { get; set; }
+    public string DescripcionPlan { get; set; } = string.Empty;
+    public string TipoPersona { get; set; } = string.Empty;
+}
