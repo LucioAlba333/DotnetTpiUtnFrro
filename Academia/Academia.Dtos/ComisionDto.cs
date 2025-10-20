fd37f9f0220d2ca1f26bf4133bd81083fd2886f4
Lucio Albanesi <lucioalba02@gmail.com>
2025-10-20 17:16:45 -0300

    agregado dto y service

---
+++ Academia/Academia.Dtos/ComisionDto.cs
@@ -0,0 +1,14 @@
+﻿namespace Academia.Dtos;
+
+public class ComisionDto
+{
+    public int Id { get; set; }
+    public string Descripcion { get; set; } = string.Empty;
+    public int AnioEspecialidad { get; set; }
+    public int IdPlan {  get; set; }
+    public string PlanDescripcion { get; set; } = string.Empty;
+
+}
+    
+    
+
--- Academia/Academia.Services/ComisionService.cs
+++ Academia/Academia.Services/ComisionService.cs
@@ -1,14 +1,40 @@
 ﻿using Academia.Data;
+using Academia.Dtos;
+using Academia.Models;
 
 namespace Academia.Services;
 
 public class ComisionService
 {
     private readonly ComisionRepository _comisionRepository;
+    private readonly PlanRepository _planRepository;
 
-    public ComisionService(ComisionRepository comisionRepository)
+
+    public ComisionService(ComisionRepository comisionRepository, PlanRepository planRepository)
     {
         _comisionRepository = comisionRepository;
+        _planRepository = planRepository;
+    }
+
+    public async Task New(ComisionDto comisionDto)
+    {
+        try
+        {
+            Plan? plan = await _planRepository.Get(comisionDto.IdPlan);
+            if (plan != null) 
+            {
+                Comision comision = new Comision(comisionDto.Id,
+                    comisionDto.Descripcion,
+                    comisionDto.AnioEspecialidad,
+                    plan);
+                await _comisionRepository.Add(comision);
+            }
+            
+        }
+        catch (Exception e)
+        {
+            throw new Exception("error al crear la comision", e);
+        }
     }
 
 }
